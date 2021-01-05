d94d3299fd21f8184e96b5a9696f351f324f9233
jantonywest <jintoantony14@gmail.com>
2021-01-05 10:42:33 -0500

    created Movie Project

---
+++ TradeDev/TradeDev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TradeDev
+//
+//  Created by Jinto Antony on 2021-01-05.
+//  Copyright © 2021 JA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
